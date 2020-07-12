7c07dc4ac182c8f431a8570d254c7a66ed8e5d3b
gavin <gavinoshc@gmail.com>
2020-07-12 15:25:58 +0100

    Updates

---
+++ precache-manifest.83b7f1f390da3e679ecf4219458e4135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60652b279d181692cbb4f853281fba78",
+    "url": "/ISS-Current-Location/index.html"
+  },
+  {
+    "revision": "ca5ed9e9d4189cb30886",
+    "url": "/ISS-Current-Location/static/css/main.925ce557.chunk.css"
+  },
+  {
+    "revision": "8c30b8a072929d192ffe",
+    "url": "/ISS-Current-Location/static/js/2.f6d27438.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ISS-Current-Location/static/js/2.f6d27438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5ed9e9d4189cb30886",
+    "url": "/ISS-Current-Location/static/js/main.7fab3098.chunk.js"
+  },
+  {
+    "revision": "214b78cc5dbcd64e967c",
+    "url": "/ISS-Current-Location/static/js/runtime-main.2f8dd297.js"
+  }
+]);
